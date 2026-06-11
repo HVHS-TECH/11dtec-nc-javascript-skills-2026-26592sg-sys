2346b372fade540d7e85e896592514bb6f7132b4
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-11 21:26:45 +0000

    Jun 11, 2026, 9:26 PM

--- t02_variables.js
+++ t02_variables.js
@@ -5,6 +5,11 @@ console.log("Running t01_introduction.js")
 console.log("Hello")
 
 // Variables
+Hi _______
+
+As of ____ you are __ years old
+
+You have __ dollars
 
 /****************************
 Main code:
