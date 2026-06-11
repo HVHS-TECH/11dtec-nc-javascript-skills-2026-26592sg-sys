8ccca414902f48671c18bd60a6455813d50fad4d
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-11 21:10:33 +0000

    Jun 11, 2026, 9:10 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,15 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of task: 
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("Hello")
 
-// This is a single line comment
+// Variables
 
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions:
+****************************/
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,15 @@
-/* Paste your code from task 1 here */
+/****************************
+Name of task:Variables
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello")
+
+// Variables
+
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions:
+****************************/
