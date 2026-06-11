be4252a7fdb7d52fab268542e70e8c30161a877b
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-11 21:54:23 +0000

    Jun 11, 2026, 9:54 PM

--- t02_variables.js
+++ t02_variables.js
@@ -9,6 +9,8 @@ var name = "Shan"
 var age = 17;
 var pocketMoney = 0;
 var currentYear = 2026; 
+var country = "Philippines"; 
+
 console.log("Hi"+name)
 console.log("As of"+currentYear+"you are"+age)
 console.log("You have"+pocketMoney) 
