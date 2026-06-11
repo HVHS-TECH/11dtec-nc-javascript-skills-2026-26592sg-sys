fa12b113e576200f19bb65621600d187263ae34d
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-11 21:36:41 +0000

    Jun 11, 2026, 9:36 PM

--- t02_variables.js
+++ t02_variables.js
@@ -5,8 +5,10 @@ console.log("Running t01_introduction.js")
 console.log("Hello")
 
 // Variables
-var name = Shan
-var age = 17
+var name = "Shan"
+var age = 17;
+var pocketMoney = 0;
+var currentYear = 2026; 
 
 /****************************
 Main code:
