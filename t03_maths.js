9faa97ac9ace3dd81dea4caad307476d3daf548a
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-14 22:19:57 +0000

    Jun 14, 2026, 10:19 PM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 2 here */
+/*/****************************
+Name of task:Variables
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello")
+
+// Variables
+var name = "Shan"
+var age = 17;
+var pocketMoney = 20;
+var currentYear = 2026; 
+var country = "Philippines"; 
+var myNumber = 27;
+var mySecNumber = 10;
+var myThirdNumber = 13;
+console.log("Hi"+name)
+console.log("As of"+currentYear+"you are"+age)
+console.log("You were born in"+country)
+
+console.log("You have"+pocketMoney) 
+
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions:
+****************************/ 
