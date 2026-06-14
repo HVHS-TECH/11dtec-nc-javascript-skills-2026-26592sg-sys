0f10e5ce75893689951a362f2225cc166ccb0a55
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-14 22:48:26 +0000

    Jun 14, 2026, 10:48 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -4,19 +4,19 @@ console.log("Task 4 - Debugging")
 var area = length * width;
 
 // calculate the perimeter
-var perimeter = 2 length + 2 * width; 
+var perimeter = 2* length + 2* width; 
 
 // Set up the size of the page.
 var length = 8;
 var width = 10;
 
 // Display the results
-console.log("An area of a piece of paper with a length of"+length+,"and a width of"+width+ "is"+ area)
-console.log("The same piece of paper has a perimeter of "+perimeter)
+console.log("An area of a piece of paper with a length of"+length+"and a width of"+width+ "is"+ area)
+console.log("The same piece of paper has a perimeter of"+perimeter)
 
 // Check, does this result look sensible
 if (perimeter > area){
     console.log("Yay, your area is larger, this is right for this example")
 }else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
