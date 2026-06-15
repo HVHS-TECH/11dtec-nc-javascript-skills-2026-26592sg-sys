841edc2d7b2c8cacf66aa12e3fa2efa594127639
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-15 05:31:39 +0000

    Jun 15, 2026, 5:31 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,30 @@
- 
+ /*/****************************
+Name of task:Variables
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello")
+
+// Variables
+var name = "Shan"
+var age = 17;
+var pocketMoney = 20;
+var currentYear = 2026; 
+var country = "Philippines"; 
+var myNumber = 27;
+var mySecNumber = 10;
+var myThirdNumber = 13;
+console.log("Hi" + name);
+console.log("As of" + currentYear + "you are" + age);
+console.log("You were born in" + country);
+console.log("In 10 years you will be" + myNumber + "years old");
+console.log("You have" + pocketMoney); 
+console.log("You spend half of your money, now you have" + mySecNumber);
+console.log("Then you get $3, now you have" + myThirdNumber);    
+
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions:
+****************************/ 
