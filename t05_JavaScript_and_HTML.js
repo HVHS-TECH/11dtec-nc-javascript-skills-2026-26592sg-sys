4d0bd22bf510eccb73ae546e2a984e2cc82740bc
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-15 05:42:27 +0000

    Jun 15, 2026, 5:42 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -13,7 +13,7 @@ var country = "Philippines";
 var myNumber = 27;
 var mySecNumber = 10;
 var myThirdNumber = 13;
-const OUTPUT = 
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 console.log("Hi" + name);
 console.log("As of" + currentYear + "you are" + age);
 console.log("You were born in" + country);
