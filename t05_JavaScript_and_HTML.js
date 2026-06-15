9fc372515851a6c39c2a2dce776193be3293260d
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-15 05:46:44 +0000

    Jun 15, 2026, 5:46 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -14,13 +14,13 @@ var myNumber = 27;
 var mySecNumber = 10;
 var myThirdNumber = 13;
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = ("Hi" + name);
+OUTPUT.innerHTML = "Hi" + name);
 OUTPUT.innerHTML += ("As of" + currentYear + "you are" + age);
-OUTPUT.innerHTML +=("You were born in" + country);
-console.log("In 10 years you will be" + myNumber + "years old");
-console.log("You have" + pocketMoney); 
-console.log("You spend half of your money, now you have" + mySecNumber);
-console.log("Then you get $3, now you have" + myThirdNumber);    
+OUTPUT.innerHTML += ("You were born in" + country);
+OUTPUT.innerHTML += ("In 10 years you will be" + myNumber + "years old");
+OUTPUT.innerHTML += ("You have" + pocketMoney); 
+OUTPUT.innerHTML += ("You spend half of your money, now you have" + mySecNumber);
+OUTPUT.innerHTML += ("Then you get $3, now you have" + myThirdNumber);    
 
 /****************************
 Main code:
