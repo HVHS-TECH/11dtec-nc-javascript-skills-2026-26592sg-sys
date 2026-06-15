a2bb0301f655ee4eecbab3bcb587d82d7c6be78c
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-15 23:43:01 +0000

    Jun 15, 2026, 11:43 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,31 @@
-/* Paste your code from task 5 here */
+/* /*/****************************
+Name of task: Task 0
+****************************/
+console.log("Running task 05...")
+console.log("Hello")
+
+// Variables
+var name = "Shan"
+var age = 17;
+var pocketMoney = 20;
+var currentYear = 2026; 
+var country = "Philippines"; 
+var myNumber = 27;
+var mySecNumber = 10;
+var myThirdNumber = 13;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<p>Hi" + name + "</p>";
+OUTPUT.innerHTML += "<p>As of" + currentYear + "you are" + age + "</p>";
+OUTPUT.innerHTML += "<p>You were born in" + country + "</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be" + myNumber + "years old" + "</p>";
+OUTPUT.innerHTML += "<p>You have" + pocketMoney + "</p>"; 
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have" + mySecNumber + "</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have" + myThirdNumber + "</p>";    
+
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions:
+****************************/ 
