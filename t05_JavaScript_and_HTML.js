c8ebff765a134e821be0ca1ebf607a09235ae704
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-15 05:36:30 +0000

    Jun 15, 2026, 5:36 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,7 +1,7 @@
  /*/****************************
 Name of task: Task 05 - JavaScript and HTML
 ****************************/
-console.log("Running t01_introduction.js")
+console.log("Running task 05...")
 console.log("Hello")
 
 // Variables
