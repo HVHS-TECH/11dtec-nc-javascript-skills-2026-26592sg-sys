b33ff7a0a2e2d214feeef822a6c57437fee78a85
26592sg-sys <26592sg@hvhs.school.nz>
2026-06-18 21:50:02 +0000

    Jun 18, 2026, 9:50 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,58 @@
-/* Paste your code from the last task */
+/*/****************************
+Name of task: Task 10 - Input from HTML
+****************************/
+console.log("Running task 10...")
+console.log("Hello")
+
+// Variables
+var name = "Shan"
+var age = 17;
+var pocketMoney = 20;
+var currentYear = 2026; 
+var country = "Philippines"; 
+var myNumber = 27;
+var mySecNumber = 10;
+var myThirdNumber = 13;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<p>Hi" + name + "</p>";
+OUTPUT.innerHTML += "<p>As of" + currentYear + "you are" + age + "</p>";
+OUTPUT.innerHTML += "<p>You were born in" + country + "</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be" + myNumber + "years old" + "</p>";
+OUTPUT.innerHTML += "<p>You have" + pocketMoney + "</p>"; 
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have" + mySecNumber + "</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have" + myThirdNumber + "</p>";    
+
+/****************************
+Main code:
+****************************/
+welcome()
+
+/****************************
+Functions:
+****************************/ 
+function welcome(){
+
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+"<p>Open the console <code>(Ctrl-shift-i)</code>"
+<button ;onClick="start()\;">START<button>
+
+<div id="spaceForJavaScriptOutput"></div>
+/****************************
+Main code:
+****************************/
+
+
+/****************************
+Functions:
+****************************/ 
+function start(){
+    spaceForJavaScriptOutput.innerHTML
+}   ;spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - age; 
+    spaceForJavaScriptOutput.innerHTML
+
+<form>
+    <input type="text">
+    <input type= "submit">
+    </form>
